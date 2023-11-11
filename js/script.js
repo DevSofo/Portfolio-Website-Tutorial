efeae56c2f43562cc87453e0c3e3a814149ffdc3
Sofoklis <sofoklismaniatopoulos@gmail.com>
2023-11-11 18:39:54 +0200

    modified

--- js/script.js
+++ js/script.js
@@ -6,11 +6,12 @@ var typed= new Typed(".typing", {
     loop:true
 })
 //Aside
-const nav=document.querySelector(".nav"),
+const nav=document.querySelector(".nav")
       navList=nav.querySelectorAll("li"),
       totalNavList=navList.length,
       allSection=document.querySelectorAll(".section"),
       totalSection=allSection.length;
+
       for(let i=0; i<totalNavList; i++){
         const a=navList[i].querySelector("a");
         a.addEventListener("click", function(){
